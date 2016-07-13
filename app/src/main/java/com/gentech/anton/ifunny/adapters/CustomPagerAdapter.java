08885507570be32e09605716594ec70abac21738
ankoma88 <anton.komarovskyi@gmail.com>
2016-07-13 09:41:03 +0300

    temp

--- app/src/main/java/com/gentech/anton/ifunny/adapters/CustomPagerAdapter.java
+++ app/src/main/java/com/gentech/anton/ifunny/adapters/CustomPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.gentech.anton.ifunny.adapters;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Parcelable;
@@ -12,12 +13,16 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.gentech.anton.ifunny.R;
 import com.gentech.anton.ifunny.models.ContentModel;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.Inflater;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by anton on 12.07.16.
@@ -26,8 +31,14 @@ public class CustomPagerAdapter extends PagerAdapter {
     public static final String TAG = CustomPagerAdapter.class.getSimpleName();
 
     Context context;
-    private int pos = 0;
+    //    private int pos = 0;
     private List<ContentModel> data = new ArrayList<>();
+
+     @Bind(R.id.iv_gallery_item)
+    ImageView ivContent;
+
+    @Bind(R.id.tvContent)
+    TextView tvContent;
 
     public CustomPagerAdapter(Context context, List<ContentModel> data) {
         this.data = data;
@@ -36,20 +47,33 @@ public class CustomPagerAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return Integer.MAX_VALUE;
+        return data.size();
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        ivContent = (ImageView) container.findViewById(R.id.iv_gallery_item);
+
+        View rootView = inflater.inflate(R.layout.item_content, container, false);
+        ButterKnife.bind(this, rootView);
+
+
+        createImage(position, container, rootView);
+
+        return rootView;
+
+
 //        ImageView view = new ImageView(context);
 
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View layout = inflater.inflate(R.layout.layout_content, container, false);
-        TextView view = (TextView) layout.findViewById(R.id.tvContent);
-        view.setText("1->" + position + " " + data.get(pos).getUrl());
+//        LayoutInflater inflater = LayoutInflater.from(context);
+//        View layout = inflater.inflate(R.layout.layout_content, container, false);
+//        TextView view = (TextView) layout.findViewById(R.id.tvContent);
+//        view.setText("1->" + position + " " + data.get(position).getUrl());
+//
+//        container.removeAllViews();
+//        container.addView(view, 0);
 
-        container.removeAllViews();
-        container.addView(view, 0);
 //        view.setScaleType(ImageView.ScaleType.FIT_XY);
 
 //        ContentModel contentModel = data.get(position);
@@ -58,19 +82,19 @@ public class CustomPagerAdapter extends PagerAdapter {
 
 //        view.setImageURI(Uri.parse(data.get(position).getUrl()));
 
-        if (pos >= data.size() -1) {
-            pos = 0;
-        } else {
-            ++pos;
-        }
+//        if (pos >= data.size() -1) {
+//            pos = 0;
+//        } else {
+//            ++pos;
+//        }
 
-        return view;
+//        return view;
     }
 
     @SuppressWarnings("deprecation")
     @Override
-    public void destroyItem(View container, int position, Object object) {
-        ((ViewPager) container).removeView((View) object);
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
     }
 
     @Override
@@ -81,6 +105,14 @@ public class CustomPagerAdapter extends PagerAdapter {
     @Override
     public Parcelable saveState() {
         return null;
+    }
+
+    private void createImage(int position, ViewGroup container, View rootView) {
+        String url = data.get(position).getUrl();
+        ivContent.setImageURI(Uri.parse(url));
+        tvContent.setText(data.get(position).getTitle());
+        container.addView(rootView);
+
     }
 
 }
